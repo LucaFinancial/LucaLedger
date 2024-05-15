8d96daea46599dd47c82d142826d1b7ceb1cf6ee
Johnathan <jwaspin@users.noreply.github.com>
2024-05-15 15:47:38 -0600

    Release v0.0.3 (#19)
    * chore: Add .DS_Store to .gitignore
    
    * chore: Add Vite configuration and package.json
    
    * chore: Update package.json with new name and homepage URL
    
    * Merge migration into release branch (#2)
    
    * Initial migration commit
    
    * chore: Update @luca-financial/luca-schema to version 1.0.1-beta.0
    
    * chore: Add .prettierrc configuration file
    
    * chore: Update @luca-financial/luca-schema to version 1.0.5
    
    * chore: Update ESLint and Prettier configurations
    
    * chore: Update @luca-financial/luca-schema to version 1.0.6
    
    * chore: Update @luca-financial/luca-schema to latest version
    
    * chore: Update package.json version to 0.0.1
    
    * Add favicon (#4)
    
    * Migration (#5)
    
    * chore: Add axios dependency to package.json
    
    * chore: yarn.lock
    
    * Initial migration commit
    
    * Add lucaSchemaConfig
    
    * Remove excess
    
    * chore: Update dependencies and configuration files
    
    * chore: Update ESLint configuration to ignore unused variables in rest siblings
    
    * Fix Format
    
    * chore: Update import paths for lucaSchema in multiple files
    
    * chore: Update luca-schema dependency to version 1.0.7-beta.2
    
    * chore: Update luca-schema dependency to version 1.0.7-beta.4
    
    * chore: Update luca-schema dependency and import paths for lucaSchema
    
    * chore: Add GitHub Pages deployment workflow (#6)
    
    * chore: Update luca-schema dependency to version 1.1.0 (#7)
    
    * chore: Update yarn installation and build commands in gh-pages-deploy.yml (#9)
    
    The code changes in the `.github/workflows/gh-pages-deploy.yml` file update the yarn installation and build commands. Instead of using the `yarn` command, the `borales/actions-yarn@v4` action is now used with the `install` and `build` commands. This ensures that the correct version of yarn is used and improves the reliability of the deployment process.
    
    Based on the recent user commits and repository commits, the commit message follows the established convention of starting with a type, followed by a colon and a brief description of the changes.
    
    * Features: Entities (#8)
    
    * 0.0.2 (#13)
    
    * 0.0.2 (#13) (#14)
    
    * New Schema Driven Dropdown (#18)
    
    * chore: Bump package version to 0.0.3
    
    * feat: Add DropdownCell component for schema-driven table

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,7 +2,15 @@ import doublePrecisionFormatString from './doublePrecisionFormatString';
 import generateColumnsFromSchema from './generateColumnsFromSchema';
 import parseFloatDoublePrecision from './parseFloatDoublePrecision';
 
+const ColumnTypeEnum = Object.freeze({
+  STRING: 'string',
+  NUMBER: 'number',
+  BOOLEAN: 'boolean',
+  CHECKBOX: 'checkbox',
+});
+
 export {
+  ColumnTypeEnum,
   doublePrecisionFormatString,
   generateColumnsFromSchema,
   parseFloatDoublePrecision,
