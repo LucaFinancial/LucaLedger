114e7a91533cf4b44ac6ae7cdaeb080ab7fa3013
Johnathan Aspinwall <jwaspin@gmail.com>
2024-12-24 16:23:44 -0500

    chore: Extend vite-env.d.ts to include environment variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_ENVIRONMENT: string;
+  readonly VITE_APP_NAME: string;
+  readonly VITE_APP_VERSION: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
