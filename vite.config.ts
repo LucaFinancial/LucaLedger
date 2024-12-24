6f6dceba38ae0941fbcc8f5d293761193757b7e0
Johnathan Aspinwall <jwaspin@gmail.com>
2024-12-24 17:57:04 -0500

    chore: Update TypeScript configuration to include path aliases and extend base config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      '@': '/src',
-    },
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
 });
