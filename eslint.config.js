bcbcb8107eca7f106417bd3cab4c98364784e3f5
Johnathan Aspinwall <jwaspin@gmail.com>
2024-12-24 17:50:56 -0500

    chore: Update ESLint configuration to support TypeScript and improve rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,14 @@
 import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 import react from 'eslint-plugin-react';
-import importPlugin from 'eslint-plugin-import';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
-import reactRedux from 'eslint-plugin-react-redux';
+import importPlugin from 'eslint-plugin-import';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import prettierPlugin, {
-  configs as prettierConfigs,
-} from 'eslint-plugin-prettier';
-import globals from 'globals';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
 export default [
   {
@@ -16,17 +16,26 @@ export default [
   },
   {
     files: ['**/*.{js,jsx,ts,tsx}'],
-
     languageOptions: {
-      ecmaVersion: 'latest', // or 2020, etc.
-      sourceType: 'module',
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
       globals: {
         ...globals.browser,
       },
     },
-
     settings: {
       'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+          project: './tsconfig.json',
+        },
         alias: {
           map: [['@', './src']],
           extensions: ['.js', '.jsx', '.ts', '.tsx'],
@@ -36,32 +45,35 @@ export default [
         version: '19.0',
       },
     },
-
     plugins: {
-      js,
+      '@typescript-eslint': tseslint,
       react,
       import: importPlugin,
       'jsx-a11y': jsxA11y,
-      'react-redux': reactRedux,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
       prettier: prettierPlugin,
     },
     rules: {
-      ...js.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
       ...react.configs.recommended.rules,
-      ...(react.configs['jsx-runtime']?.rules || {}),
       ...importPlugin.configs.recommended.rules,
       ...jsxA11y.configs.recommended.rules,
-      ...reactRedux.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      ...prettierConfigs.recommended.rules,
-      'no-undef': 'error',
-      'no-unused-vars': ['error', { ignoreRestSiblings: true }],
-      'react-refresh/only-export-components': 'warn',
+      ...prettierConfig.rules,
       'react/jsx-filename-extension': [
         'error',
-        { extensions: ['.js', '.jsx'] },
+        { extensions: ['.jsx', '.tsx'] },
+      ],
+      'import/extensions': [
+        'error',
+        'ignorePackages',
+        {
+          js: 'never',
+          jsx: 'never',
+          ts: 'never',
+          tsx: 'never',
+        },
       ],
     },
   },
