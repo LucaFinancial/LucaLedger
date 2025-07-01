ed9493d7a9ea9ac1fbd0018174d24c8f71b38fa5
Johnathan Aspinwall <jwaspin@gmail.com>
2025-06-30 19:10:31 -0600

    fix: Update base URL in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://storage.googleapis.com/luca-ledger-dev-web-app-bucket/',
   plugins: [react()],
   resolve: {
     alias: {
