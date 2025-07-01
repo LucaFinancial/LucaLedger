bd5c19d571dd340db3cc84508942db9360160485
Johnathan Aspinwall <jwaspin@gmail.com>
2025-06-30 19:04:26 -0600

    fix: Update homepage URL in package.json and add website configuration in Terraform

--- terraform/modules/storage/main.tf
+++ terraform/modules/storage/main.tf
@@ -4,4 +4,17 @@ resource "google_storage_bucket" "luca_ledger_web_app_bucket" {
   location                    = var.region
   force_destroy               = true
   uniform_bucket_level_access = true
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+}
+
+resource "google_storage_bucket_iam_binding" "public_access" {
+  bucket = google_storage_bucket.luca_ledger_web_app_bucket.name
+
+  role = "roles/storage.objectViewer"
+
+  members = [ "allUsers" ]
 }
