d6c58880ca156aa025d5f0a8e50171111614feb0
Johnathan Aspinwall <jwaspin@gmail.com>
2025-07-11 08:12:39 -0600

    refactor: Remove public access IAM member from Cloud Run service account

--- terraform/modules/iam/cloudrun.tf
+++ terraform/modules/iam/cloudrun.tf
@@ -3,10 +3,3 @@ resource "google_service_account" "cloudrun_sa" {
   account_id   = "cloudrun-sa"
   display_name = "Cloud Run Service Account"
 }
-
-resource "google_cloud_run_service_iam_member" "public_access" {
-  location = var.region
-  service  = var.service_name
-  role     = "roles/run.invoker"
-  member   = "allUsers"
-}
