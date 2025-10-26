2b303f30b26fc535447ee64d1a580d3a1e20806d
Johnathan Aspinwall <jwaspin@gmail.com>
2025-10-26 17:24:43 -0600

    refactor: remove unused category ID from transaction generator

--- src/store/transactions/generators.js
+++ src/store/transactions/generators.js
@@ -2,14 +2,8 @@ import dayjs from 'dayjs';
 import { v4 as uuid } from 'uuid';
 
 import config from '@/config.js';
-import categoriesData from '@/config/categories.json';
 import { TransactionStatusEnum } from './constants';
 import { validateTransactionSync } from '@/validation/validator';
-
-// Get the None category ID from config
-const NONE_CATEGORY_ID = categoriesData.categories.find(
-  (cat) => cat.slug === 'none'
-)?.id;
 
 export const generateTransaction = (initialData = {}) => {
   const transaction = {
@@ -19,7 +13,6 @@ export const generateTransaction = (initialData = {}) => {
     date: dayjs().format(config.dateFormatString),
     amount: 0,
     description: 'Enter transaction description',
-    categoryId: NONE_CATEGORY_ID,
     ...initialData,
   };
 
