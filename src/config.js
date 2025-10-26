b052b0ed58f0ce577201b6713ef0f61aee3ee15a
Johnathan Aspinwall <jwaspin@gmail.com>
2025-10-26 17:22:09 -0600

    refactor: consolidate config export and include categories data

--- src/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  monthFormatString: 'MMMM YYYY',
-  dateFormatString: 'YYYY/MM/DD',
-  compareDateFormatString: 'YYYY/MM/DD',
-  legalDisclaimer:
-    'Disclaimer: The software provided on this website is provided "as is" without any warranties or guarantees. Use it at your own risk. The authors and contributors disclaim any liability for any damages or losses related to the use of the software.',
-};
-
-export default config;
