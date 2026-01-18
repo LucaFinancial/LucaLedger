01ef0180f9e83cb81e2c885f0e1ca8b65f272d84
Johnathan Aspinwall <jwaspin@gmail.com>
2026-01-17 23:19:13 -0700

    Merge branch 'main' into release/2-1-0

--- src/components/Auth/index.js
+++ src/components/Auth/index.js
@@ -2,4 +2,3 @@ export { default as AuthScreen } from './AuthScreen';
 export { default as LoginForm } from './LoginForm';
 export { default as RegisterForm } from './RegisterForm';
 export { default as ProtectedRoute } from './ProtectedRoute';
-export { default as LegacyDataMigrationDialog } from './LegacyDataMigrationDialog';
