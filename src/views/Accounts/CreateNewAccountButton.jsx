d74fd3b8ee0692219b8ce40cd655dff604582e21
Johnathan Aspinwall <jwaspin@gmail.com>
2026-02-01 20:17:43 -0700

    fix: rename CreateNewButton to CreateNewAccountButton and add component implementation

---
+++ src/views/Accounts/CreateNewAccountButton.jsx
@@ -0,0 +1,24 @@
+import { Button } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { actions, selectors } from '@/store/accounts';
+
+export default function CreateNewAccountButton() {
+  const dispatch = useDispatch();
+  const loading = useSelector(selectors.selectAccountsLoading);
+
+  const handleCreateAccount = () => {
+    dispatch(actions.createNewAccount());
+  };
+
+  return (
+    <Button
+      variant='contained'
+      color='primary'
+      onClick={handleCreateAccount}
+      disabled={loading}
+    >
+      Create New Account
+    </Button>
+  );
+}
